1cebd156edc250a52ae620fed852d841ea5b56f6
Andrew Halls <andrew@galtsoft.com>
2014-09-17 13:42:14 +0800

    Adding Swift Tests

---
+++ XbICalendar/XbICalendarSwiftTests/XbICalendarSwiftTests.swift
@@ -0,0 +1,20 @@
+//
+//  XbICalendarSwiftTests.swift
+//  XbICalendarSwiftTests
+//
+//  Created by Andrew Halls on 9/17/14.
+//  Copyright (c) 2014 GaltSoft. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class XbICalendarSwiftTests: XCTestCase {
+
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssertTrue(true == false, "Pass")
+    }
+
+}
